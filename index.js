a54ca8f41b2e37bfda56650c73d1b27e8c5ce387
mvazquez <matias.vazquezmartin@gmail.com>
2024-01-20 21:34:46 -0300

    feat: reaction new user

---
+++ index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+
+const reactions = ["🌈", "🏳‍🌈", "👬", "💅", "🍌"];
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.on("messageCreate", (msg) => {
+  console.log(msg);
+  if (msg.system && msg.type === 7) {
+    const randomReaction =
+      reactions[Math.floor(Math.random() * reactions.length)];
+    msg.react(randomReaction).catch(console.error);
+  }
+});
+
+client.login(process.env.TOKEN);
