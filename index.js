33f6c4865719b89a7a9919daa21f4a1f2c6464f4
mvazquez <matias.vazquezmartin@gmail.com>
2024-01-20 23:28:16 -0300

    feat: add setInterval

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 require("dotenv").config();
+const express = require("express");
 const { Client, Events, GatewayIntentBits } = require("discord.js");
 
-const reactions = ["🌈", "🏳‍🌈", "👬", "💅", "🍌"];
+const reactions = ["🌈", "🏳‍🌈", "👬", "💅", "🍌", ""];
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Pong!");
+});
+
+function sendPing() { 
+  fetch("https://hellopridebot.glitch.me/")
+    .then((response) => response.text())
+    .then((data) => console.log(data))
+    .catch((error) => console.error("Error al enviar ping:", error));
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 const client = new Client({
   intents: [
@@ -13,9 +31,11 @@ const client = new Client({
 
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+
+  setInterval(sendPing, 1800000);
 });
 
-client.on("messageCreate", (msg) => {  
+client.on("messageCreate", (msg) => {
   if (msg.system && msg.type === 7) {
     const randomReaction =
       reactions[Math.floor(Math.random() * reactions.length)];
