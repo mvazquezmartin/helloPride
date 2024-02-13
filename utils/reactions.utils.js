4ba079865257f3079378ceb75e9dcc941ebac298
mvazquez <matias.vazquezmartin@gmail.com>
2024-02-13 17:40:33 -0300

    style: minor changes

--- utils/reactions.utils.js
+++ utils/reactions.utils.js
@@ -3,7 +3,7 @@ const reactions = (returnSlice) => {
   const shuffledReactions = reactionsEmoji.sort(() => Math.random() - 0.5);
 
   if (returnSlice) {
-    const numberSlice = Math.floor(Math.random() * reactionsEmoji.length) + 1;
+    const numberSlice = 2; //Math.floor(Math.random() * reactionsEmoji.length) + 1;
     return shuffledReactions.slice(0, numberSlice);
   }
   return shuffledReactions;
